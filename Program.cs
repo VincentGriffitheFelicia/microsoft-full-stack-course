a3c106174e45466d4fff6c987212b32c413d237a
VincentGriffitheFelicia <feliciavincentgriffithe@gmail.com>
2025-11-08 09:15:51 +0800

    Registered the DataService as a singleton service

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using Course_Repository;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddSingleton<DataService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
