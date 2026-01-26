3e2f1de708c8981fe718fd02157fb89d5e8a5630
VincentGriffitheFelicia <feliciavincentgriffithe@gmail.com>
2026-01-26 18:39:29 +0800

    Finished

--- obj/Debug/net9.0/Course Repository.AssemblyInfo.cs
+++ obj/Debug/net9.0/Course Repository.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Course Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cc8c816786473606e66702137aac80dcf9d16de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+596e4f160adf09869eb6286f5f74a3dc1144ec08")]
 [assembly: System.Reflection.AssemblyProductAttribute("Course Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Course Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
