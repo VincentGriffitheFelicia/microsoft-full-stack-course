520b3eb4f07626c620e0a1a6f2aca2218fb327fd
VincentGriffitheFelicia <feliciavincentgriffithe@gmail.com>
2026-01-27 18:01:10 +0800

    Done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+class Program
+{
+    public static void Main()
+    {
+
+        Person person = new Person { UserName = "John Doe", UserAge = 25 };
+        string serialized = JsonSerializer.Serialize(person);
+
+        Console.WriteLine(serialized);
+    }
+
+    class Person
+    {
+        required public string UserName { get; set; }
+        public int? UserAge { get; set; }
+    }
+}
