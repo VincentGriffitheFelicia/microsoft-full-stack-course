a243a30168bd4ca1e0a9b797369e59b13959449f
VincentGriffitheFelicia <feliciavincentgriffithe@gmail.com>
2026-01-28 13:45:12 +0800

    Done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MyClientNamespace;
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var httpClient = new HttpClient();
+        var client = new MyClientNamespace.OctoberApiClient("http://localhost:5000", httpClient);
+        var user = await client.UserAsync(1);
+        Console.WriteLine(user);
+    }
+}
