7611131f3ea2d57ddb7e1e1b6b1cb47fd5e41f51
VincentGriffitheFelicia <feliciavincentgriffithe@gmail.com>
2026-01-30 14:59:13 +0800

    Done

---
+++ Models/Employee.cs
@@ -0,0 +1,10 @@
+
+public class Employee
+{
+    public int EmployeeID { get; set; } // Primary Key
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime HireDate { get; set; }
+    public int DepartmentID { get; set; } // Foreign Key
+    public Department Department { get; set; } // Navigation Property
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+
+var context = new HRDbContext();
+
+var departments = context.Departments.ToList();
+var employees = context.Employees.ToList();
+var employeesWithDepartment = context.Employees.Include(e => e.Department);
+
+var options = new JsonSerializerOptions
+{
+    ReferenceHandler = ReferenceHandler.Preserve,
+    WriteIndented = true
+};
+
+Console.WriteLine("=================DEPARTMENTS===============");
+foreach (var dep in departments)
+{
+    var json = JsonSerializer.Serialize(dep, options);
+    Console.WriteLine(json);
+}
+
+Console.WriteLine("=================EMPLOYEES===============");
+foreach (var emp in employees)
+{
+    var json = JsonSerializer.Serialize(emp, options);
+    Console.WriteLine(json);
+}
+
+Console.WriteLine("=================EMPLOYEES WITH DEPARTMENT===============");
+foreach (var emp in employeesWithDepartment)
+{
+    var json = JsonSerializer.Serialize(emp, options);
+    Console.WriteLine(json);
+}
